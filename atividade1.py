7b7d630ef7c1074353234a70fc6ef10fe64987a6
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2024-04-17 15:59:25 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,4 +1,4 @@
-"""Crie um programa que receba do usuário separadamente o nome, o sobrenome, o dia, o mês (número) e o ano de nascimento. 
-Em seguida, mostre na tela o seguinte resultado:
-Nome: nome sobrenome
-Data de Nascimento: dia/mês/ano"""
+#Crie um programa que receba do usuário separadamente o nome, o sobrenome, o dia, o mês (número) e o ano de nascimento. 
+#Em seguida, mostre na tela o seguinte resultado:
+#  Nome: nome sobrenome
+#  Data de Nascimento: dia/mês/ano
