aec8e0c1e31ccfd5cb451ef8b4be06bbc1773f23
Gustavo Nunes Lopes <gustaavo.nunesl@gmail.com>
2024-04-23 17:59:37 -0300

    Gabarito da atividade 01

--- atividade1.py
+++ atividade1.py
@@ -3,3 +3,17 @@
 # Em seguida, mostre na tela o seguinte resultado:
 # Nome: nome sobrenome
 # Data de Nascimento: dia/mês/ano
+
+# Defina se irá usar aspas simples ou duplas no seu código, para que fique
+# padronizado, misturamos para que você veja que da certo com ambas.
+nome = input('Digite o seu nome: ')
+sobrenome = input("Digite o seu sobrenome: ")
+# Você não precisa transformar o dia/mes/ano para int, pois
+# não iremos fazer nenhuma operação aritmética com os dados informados
+# poderia ser tudo padrão str.
+dia_de_nascimento = int(input('Digite o seu dia de nascimento: ')) 
+mes_de_nascimento = int(input("Digite o seu mes de nascimento: "))
+ano_de_nascimento = int(input('Digite o seu ano de nascimento: '))
+
+print(f"Nome: {nome} {sobrenome}")
+print(f"Data de Nascimento: {dia_de_nascimento}/{mes_de_nascimento}/{ano_de_nascimento}")
