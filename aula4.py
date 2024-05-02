9c7dfb1535ae61ad80c78049ab07f5f97c49d7b0
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2024-05-02 16:25:43 -0300

    Update aula4.py

--- aula4.py
+++ aula4.py
@@ -1,7 +1,7 @@
 # Podemos receber dados do usuário através da função input
 # Por padrão chega tudo como str
 
-idade = input('Digite a sua idade: ')
-idade_convertida = int(idade)
+idade = input('Digite a sua idade: ') # idade chega como uma string
+idade_convertida = int(idade) # idade_convertida recebe o valor de idade mas na forma de um inteiro
 print(type(idade_convertida))
 print(type(idade))
