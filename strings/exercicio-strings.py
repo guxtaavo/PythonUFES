6031739f72f8960973c018e33de6157a25b8ebec
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2024-05-02 23:52:51 -0300

    Update exercicio-strings.py

--- strings/exercicio-strings.py
+++ strings/exercicio-strings.py
@@ -1,10 +1,15 @@
-# Crie um programa que receba uma string com o nome do funcionario, sua função e seu salario:
-# Ex:
-"""João - Gerente - 5000
-Jorge - Caixa - 2000
-Fernanda - Caixa - 2000
-Pedro - Padeiro - 3000"""
-# E retorne quantos funcionarios trabalham em cada funcao e qual gasto total com o salário dos funcionarios
-# Saida: 
-"""Função: nº de Funcionarios
-Gasto: Soma dos salários"""
+# Crie um programa que receba do usuário um conjunto de e-mails, 
+# retorne se o formato recebido é válido, e caso seja, retorne também qual o serviço de e-mail utilizado
+# Um email só será válido se for no formato nome@serviço.tipo e terminar com .com ou .br
+# Ex: fulano@gmail.com - Saída: Válido - gmail
+
+emails = input("Digite os emais separados por um espaço:\n")
+emails = emails.split()
+for email in emails:
+    if ("@" in email and (email.endswith(".com") or email.endswith(".br"))):
+        email = email.split("@")[1]
+        print("Válido", email[email.find("@")+1:email.find(".")])
+    else:
+        print("Inválido")
+        continue
+
