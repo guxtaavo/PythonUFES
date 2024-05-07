590b223cc6302094395fa51357b644cc79de9962
Gustavo Nunes Lopes <gustaavo.nunesl@gmail.com>
2024-05-06 21:41:35 -0300

    Atualizando o código do dict

--- colecoes/dicionarios.py
+++ colecoes/dicionarios.py
@@ -7,7 +7,7 @@
 # dicionários.
 # Imutáveis: str, int, float, bool, tuple
 # Mutável: dict, list
-#
+# https://docs.python.org/pt-br/3/tutorial/datastructures.html#dictionaries
 
 
 # lista = ['Gustavo', 20, 'UFES', 'ES']
@@ -20,6 +20,19 @@ pessoa = {
     ]
 }
 
-pessoa.pop("nome")
-print(pessoa)
+# Método GET -> retorna o valor da chave, pode passar um valor caso não encontre
+# print(pessoa.get('nome', 'pode passar algo aqui opcional, por padrão = None'))
+
+# Método KEYS -> retorna as chaves do dict
+# print(pessoa.keys())
+
+# Método VALUES -> retorna as valores do dict
+# print(pessoa.values())
+
+# Método clear -> Limpa o conteúdo do dict
+# pessoa.clear()
+
+# Método items -> Retorna o conjunto de chave e valor do dict em forma de tupla
+# print(pessoa.items())
+
 
