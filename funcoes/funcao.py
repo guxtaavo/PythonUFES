38d31cd3669ea1a64c2a6922b2b81c298adb7a84
Gustavo Nunes Lopes <gustaavo.nunesl@gmail.com>
2024-05-07 09:40:59 -0300

    Comentando o código de funções

--- funcoes/funcao.py
+++ funcoes/funcao.py
@@ -8,9 +8,19 @@ def imprime_msg(x, y, z):
     #QUALQUER LÓGICA
     print(x, y , z)
 
+    # As váriaveis dentro da função, só existem dentro dela, e não existem no escopo geral do código, são váriaveis temporarias
+    # Só irão existir enquanto o código estiver no escopo da função
+
+
+    # A função pode retornar algo, que voce armazenaria em alguma variavel ou utilizaria em alguma coisa
+    # Ou não precisa retornar nada
+    # Pode retornar uma outra função também, dependendo da sua lógica dentro do código
+    # return
+
 n1 = 1
 n2 = 2
 n3 = 3
+
 #Exemplo de argumentos nomeados
 imprime_msg(1, z=n3, y=n2)
 
