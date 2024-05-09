b5d0a1b627a69beba2ce94b37a00e4202b4c6962
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2024-05-09 20:47:23 -0300

    Update aula-strings2.py

--- strings/aula-strings2.py
+++ strings/aula-strings2.py
@@ -3,3 +3,20 @@
 # Strip
 # Replace
 # Split
+
+# O upper deixa todas as letras maiúsculas / O lower deixa todas as letras minúsculas
+X = "Hello, World123?@"
+Y = X.lower()
+print(Y)
+
+# O strip remove os espaços em branco no início e no final (Se colocarmos alguma string entre parênteses, essa string que será removida das extremidades)
+Blank = "  Qualquer Coisa   "
+print(Blank.strip())
+
+# O replace serve para substituir uma string por outra
+Z = X.replace("123?@","")
+print(Z)
+
+# O split serve para separar uma string em uma lista de strings (Se deixarmos vazio entre parênteses, iremos separar os espaços vazios " ")
+W = Z.split(",")
+print(W)
