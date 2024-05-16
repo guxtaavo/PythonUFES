ff187096af6c6d6481027806cba0c06d90d50756
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2024-05-16 17:07:41 -0300

    Update aula-strings3.py

--- strings/aula-strings3.py
+++ strings/aula-strings3.py
@@ -6,17 +6,18 @@
 # Endswith / Startswith
 
 # O capitalize deixa apenas a primeira letra da string maiúscula, e o restante minúscula
-X = "12 345"
+X = "Hello, World"
 print(X.capitalize())
 
 # O count conta quantas vezes um caracter se repete
-contagem = X.count("L")
+contagem = X.count("l")
 print(contagem)
 
 # find encontra qual o índice em que um caracter aparece pela primeira vez
 print(X.find("l"))
 
 # isalpha pergunta se todos os caracteres são letras e isnumeric pergunta se todos os caracteres são números
+X = "12 345"
 print(X.isnumeric())
 
 # startswith pergunta qual o começo de uma string e endswith pergunta qual o final
