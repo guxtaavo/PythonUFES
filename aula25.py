b05170a63d138e7ee32788c037b1110ceae4b238
Gustavo Nunes <109112700+guxtaavo@users.noreply.github.com>
2024-05-17 11:44:33 -0300

    Update aula25.py

--- aula25.py
+++ aula25.py
@@ -11,3 +11,12 @@ import matplotlib.pyplot as plt
 plt.plot([1, 4], [1, 16]) # Associação ponto a ponto
 plt.axis((0, 6, 0, 20)) # Intervalo dos eixos
 plt.show()
+
+# Múltiplos gráficos
+grafico1 = [3, 8, 1, 10]
+grafico2 = [6, 2, 7, 11]
+
+plt.plot(grafico1)
+plt.plot(grafico2)
+
+plt.show()
