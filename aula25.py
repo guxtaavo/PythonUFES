8f9ac0288e136d0019f38e83368fc8cadf114add
Yago2611 <116475207+Yago2611@users.noreply.github.com>
2024-05-24 02:48:55 -0300

    Update aula25.py

--- aula25.py
+++ aula25.py
@@ -3,14 +3,12 @@
 import matplotlib.pyplot as plt
 
 # Plotar gráfico só com o valor de Y
-# plt.plot([1, 2, 3, 4]) 
+plt.plot([1, 2, 3, 4]) 
 #       x = 0  1  2  3     
-# plt.show()
 
 # Passando eixo X e eixo Y
 plt.plot([1, 4], [1, 16]) # Associação ponto a ponto
 plt.axis((0, 6, 0, 20)) # Intervalo dos eixos
-plt.show()
 
 # Múltiplas Linhas
 linha1 = [3, 8, 1, 10]
