9e8c8713367bea5c47743e83e656cc7f87416f2a
Gustavo Nunes <109112700+guxtaavo@users.noreply.github.com>
2024-06-25 14:25:19 -0300

    Create aula1.py

---
+++ calculadora/aula1.py
@@ -0,0 +1,9 @@
+# Aula 1: Apresentação do projeto e configuração do ambiente
+import tkinter as tk
+
+# Criação da janela principal
+root = tk.Tk()
+root.title("Calculadora")
+
+# Loop da aplicação
+root.mainloop()
